a91cf8d1c6944b9d1f590457b0a8d1d9ccc0286c
Ammar <ammarrawash0@gmail.com>
2023-02-16 10:45:42 -0800

    Task 8:

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,17 @@
+#include<unistd.h>
+#include<stdio.h>
+
+/**
+ *main - print a string
+ *
+ *Return: Always 1 (Success)
+ */
+
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" \
+- Dora Korpar, 2015-10-19\n");
+
+return (1);
+
+}
