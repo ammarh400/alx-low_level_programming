55e941985f589f2d061272715dc953b44e4a8c72
Ammar <ammarrawash0@gmail.com>
2023-02-21 05:12:14 -0800

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include"main.h"
+
+/**
+ *_abs - print abs
+ *@n: return the abs_value
+ *Return: Absolute value of n or 0
+ */
+
+int _abs(int n)
+{
+	return (abs(n));
+}
+
