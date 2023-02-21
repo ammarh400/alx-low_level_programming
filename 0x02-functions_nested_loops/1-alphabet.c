d7b2da31a42c6277a0cdd8ed99f9d4fcfede718a
Ammar <ammarrawash0@gmail.com>
2023-02-21 00:20:39 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ *main - print alphabet
+ *
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char ch ='a';
+
+       for (ch = 'a'; ch <= 'z'; ; ch++)
+       {
+	       _putchar(ch);
+
+       }
+
+       return (0);
+}
