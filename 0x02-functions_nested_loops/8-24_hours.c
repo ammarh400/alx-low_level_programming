61f46c3c6f6470643dd17b41bd6f6be5c13dac02
Ammar <ammarrawash0@gmail.com>
2023-02-21 06:08:44 -0800

    Task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ *jack_bauer - print Jack Bauer day
+ *
+ *Return: void
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (j = 0; j < 60; j++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			_putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+
+}
