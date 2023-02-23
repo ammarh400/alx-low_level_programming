0e4ba5f7d7c1b4dd416eacbc40888f7da9bfcb7f
Ammar <ammarrawash0@gmail.com>
2023-02-23 03:29:15 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ *mul - muliply tow number
+ *@a: number 1
+ *@b: number 2
+ *Return: return result
+ */
+
+int mul(int a, int b)
+{
+	return ((long)(a * b));
+}
+
