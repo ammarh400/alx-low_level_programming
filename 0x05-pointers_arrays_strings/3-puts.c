15b1e702cd157932430bd9b3e5ce885b63a4db5f
Ammar <ammarrawash0@gmail.com>
2023-02-26 22:52:37 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ *_puts - print the string
+ *@str: pointer to char
+ *Return: void
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
