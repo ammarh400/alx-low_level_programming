2a99303d909f75faf7f68428c50fb7c7da923918
Ammar <ammarrawash0@gmail.com>
2023-03-02 06:48:41 -0800

    Task 9

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ *print_number - print integer
+ *
+ *@n: number to print
+ *Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int n1;
+
+	n1 = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n1 = -n;
+	}
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar(n1 % 10 + '0');
+}
