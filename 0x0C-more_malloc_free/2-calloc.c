0e5b1c538df4ac7e766037f35cc74157a32ae76e
Ammar <ammarrawash0@gmail.com>
2023-03-16 05:43:39 -0700

    Task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,7 +15,12 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	void *ptr;
 
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
 	ptr = calloc(nmemb, size);
+	if (ptr == NULL)
+		return (NULL);
 
 	return (ptr);
 }
