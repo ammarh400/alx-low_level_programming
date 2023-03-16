f6d9fd5b4e09905683ba7370558a7e0896e5be0b
Ammar <ammarrawash0@gmail.com>
2023-03-16 05:39:22 -0700

    Task 2

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include"main.h"
+
+/**
+ *_calloc - allocate using calloc
+ *@nmemb: no of arra member
+ *@size: size of array
+ *Return: pointer to void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+
+	void *ptr;
+
+	ptr = calloc(nmemb, size);
+
+	return (ptr);
+}
