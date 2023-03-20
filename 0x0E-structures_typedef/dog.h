c2eb951da0c97f6d0895e3d2180428a7a2e2a281
Ammar <ammarrawash0@gmail.com>
2023-03-20 04:08:21 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - a dog's info
+ *@name: dog's name
+ *@age: dog's age
+ *@owner: name of the owner
+ *Description: structure
+ */
+
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
